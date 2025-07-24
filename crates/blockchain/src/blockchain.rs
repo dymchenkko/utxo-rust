003672c63af6aeb74e73b3c3ff82009853c5ef00
dymchenkko <elizavetadymchenko20@gmail.com>
2025-07-24 12:04:37 +0200

    fix genesis problem

--- crates/blockchain/src/blockchain.rs
+++ crates/blockchain/src/blockchain.rs
@@ -1,6 +1,7 @@
 use crate::block::Block;
 use common::transaction::Transaction;
 use std::collections::HashSet;
+use wallet::wallet::Wallet;
 
 pub struct Blockchain {
     pub blocks: Vec<Block>,
@@ -9,10 +10,19 @@ pub struct Blockchain {
 
 impl Blockchain {
     pub fn new() -> Self {
-        let genesis_block = Block::new(String::from("0").repeat(64), vec![]);
-        let utxos = HashSet::new();
-        // In a real implementation, the genesis block might contain initial transactions
-        // that create the first UTXOs. For simplicity, we start with an empty set.
+        let faucet = Wallet::new();
+        let wallet = Wallet::new();
+        let coinbase_tx = Transaction::new(faucet.verifying_key, wallet.verifying_key, 1000, 0);
+
+        let genesis_block = Block::new(String::from("0").repeat(64), vec![coinbase_tx.clone()]);
+        let mut utxos = HashSet::new();
+        let output_utxo = format!(
+            "{}-{}",
+            hex::encode(coinbase_tx.receiver.as_bytes()),
+            coinbase_tx.nonce
+        );
+        utxos.insert(output_utxo);
+
         Self {
             blocks: vec![genesis_block],
             utxos,
@@ -30,8 +40,6 @@ impl Blockchain {
         // Update UTXO set based on the transactions in the new block.
         for tx in &block.transactions {
             // For each transaction, find the UTXOs it spends
-            // This is a simplified model. A real implementation would need to
-            // resolve transaction inputs to specific UTXOs.
             let input_utxo = format!("{}-{}", hex::encode(tx.sender.as_bytes()), tx.nonce - 1);
             if !self.utxos.contains(&input_utxo) {
                 println!("Input UTXO not found or already spent: {}", input_utxo);
